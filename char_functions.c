e1234e38aa7bc838945b9a3abc13bb8aa5736852
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-07-10 15:15:11 -0700

    Correcting the placement mistake

--- char_functions.c
+++ char_functions.c
@@ -42,41 +42,7 @@ int print_string(va_list s)
 	return (count);
 }
 
-<<<<<<< HEAD
-/**
- * print_unsigned_int -prints out binary number from unsigned integer
- *
- * @b: unsigned integer to be printed out.
- *
- * Return: number of digits printed.
- */
-int print_unsigned_int(va_list b)
-{
-	unsigned int x, y, z, sum;
-	unsigned int arr[32];
-	int count;
 
-	x = va_arg(b, unsigned int);
-	y = 2147483648;
-	arr[0] = x / y;
-	for (z = 1; z < 32; z++)
-	{
-		y /= 2;
-		arr[z] = (x / y) % 2;
-	}
-	for (z = 0, sum = 0, count = 0; z < 32; z++)
-	{
-		sum += arr[z];
-		if (sum || z == 31)
-		{
-			_putchar('0' + arr[z]);
-			count++;
-		}
-	}
-	return (count);
-}
-
-=======
 /* FOR NUMBERS && DIGITS */
 
 /**
